cbf27015bd5ec1ea08e97547e7399359a57d291d
Slynax <remlouplay3@gmail.com>
2026-02-28 18:58:51 +0100

    delete unused files

--- src/services/VSkinBackend/tradeCheck/getTradeCheck.ts
+++
@@ -1,9 +0,0 @@
-import {post} from "../../index";
-import {RequestInterfaceBody, ResponseInterface} from "./types";
-
-export const getTradeCheck = ({body}: {body: RequestInterfaceBody}) => {
-    return post<ResponseInterface>({
-        url: "https://vskin-back-production.up.railway.app/api/v1/public/extension/trade/check",
-        body,
-    });
-};
